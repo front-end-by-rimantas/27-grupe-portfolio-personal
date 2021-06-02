534738febac47f574a96938411d0fb8d739f4a98
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-06-02 14:17:28 +0300

    sidabar JS

--- js/components/header/Header.js
+++ js/components/header/Header.js
@@ -1,11 +1,30 @@
-
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
+function header() {
+    const hamburger = document.querySelector('.hamburger')
+    const close = document.querySelector('.sidebarClose')
+    let isSidebarVisible = false;
+    hamburger.addEventListener('click', () => {
+        toggleSidebar(isSidebarVisible)
+        isSidebarVisible = !isSidebarVisible;
+        hamburger.style.display = 'none';
+        close.style.display = 'block';
+    })
+    close.addEventListener('click', () => {
+        toggleSidebar(isSidebarVisible)
+        isSidebarVisible = !isSidebarVisible;
+        hamburger.style.display = 'block';
+        close.style.display = 'none';
+    })
 }
 
 
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+function toggleSidebar(isVisible) {
+    const headerSidebar = document.getElementById('headerSidebar');
+    if (isVisible) {
+        headerSidebar.style.width = "0";
+    }
+    else {
+        headerSidebar.style.width = "250px";
+    }
 }
 
-export { sidebar }
+export { header }
--- js/home.js
+++ js/home.js
@@ -1,15 +1,5 @@
 // IMPORT
-import { sidebar } from './components/header/header.js';
+import { header } from './components/header/Header.js';
 // EXECUTION
 
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
+header();

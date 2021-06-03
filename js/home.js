19908b6ce9c16b4e9d35cb82d83e666b2bc00c85
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-06-03 09:26:38 +0300

    nothing

--- js/home.js
+++ js/home.js
@@ -4,10 +4,4 @@ import { header } from './components/header/Header.js';
 // EXECUTION
 
 header();
-navBarShadow = document.getElementById(header);
-document.addEventListener('scroll', () => {
-    if (scrollY > 40) {
-        navBarShadow.classList.add('activ');
-    }
-}
-)
+

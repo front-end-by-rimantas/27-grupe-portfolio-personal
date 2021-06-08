ae877f9b57d1c50c59f0a2c4d555016e9a63d00a
mantodinas <82802179+mantodinas@users.noreply.github.com>
2021-06-09 00:43:24 +0300

    incomplete email validation js

--- js/components/footer/Footer.js
+++ js/components/footer/Footer.js
@@ -2,12 +2,12 @@ function validateEmail(inputText) {
     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (inputText.value.match(mailformat)) {
         alert("valid email address!");
-        document.emailForm.focus();
+        emailForm.focus();
         return true;
     }
     else {
         alert("invalid email address!");
-        document.emailForm.focus();
+        emailForm.focus();
         return false;
     }
 }

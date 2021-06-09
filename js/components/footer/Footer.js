22adb07476317d03577d89dc6a1265e2768a5579
mantodinas <82802179+mantodinas@users.noreply.github.com>
2021-06-09 23:37:38 +0300

    rewriting footer js

--- js/components/footer/Footer.js
+++ js/components/footer/Footer.js
@@ -15,29 +15,30 @@ function validateEmail(inputText) {
 
 function renderFooter(selector) {
     const DOM = document.querySelector(selector);
-    const HTML = `<div class="col-12 col-sm-6 col-lg-5 footerAbout">
-            <h4>About Me</h4>
-            <p>We have tested a number of registry fix and clean utilities and present our top 3 list on our site for your convenience</p>
-            <p> Copyrights &copy;2020 All rights reserved|This template is made with ♥ By <a href="">Colorlib</a></p>
+    const HTML =
+    /*`<div class="col-12 col-sm-6 col-lg-5 footerAbout">
+<h4>About Me</h4>
+<p>We have tested a number of registry fix and clean utilities and present our top 3 list on our site for your convenience</p>
+<p> Copyrights &copy;2020 All rights reserved|This template is made with ♥ By <a href="">Colorlib</a></p>
+</div>
+<div class="col-12 col-sm-6 col-lg-5 ">
+<h4>Newsletter</h4>
+<p>Stay update with uor latest trends</p>
+<form class="col-12 col-sd-6 ml-md-1 emailForm ">
+    <input class="emailFooter" placeholder="Enter Email Adress" type="email" required>
+        <button class="btnFooter" type="submit"><i class="fa fa-arrow-right"></i></button>
+    </form>
+</div>
+<div class="col-12 col-sm-12 col-lg-2 ">
+    <div class="footerFollow">
+        <h4>Follow Me</h4>
+        <p>Let us bes social</p>
+        <div class="footerSocials">
+            <a href="#" class="fa fa-facebook" target="_blank"></a>
+            <a href="#" class="fa fa-twitter" target="_blank"></a>
+            <a href="#" class="fa fa-dribbble" target="_blank"></a>
+            <a href="#" class="fa fa-behance" target="_blank"></a>
         </div>
-        <div class="col-12 col-sm-6 col-lg-5 ">
-            <h4>Newsletter</h4>
-            <p>Stay update with uor latest trends</p>
-            <form class="col-12 col-sd-6 ml-md-1 emailForm ">
-                <input class="emailFooter" placeholder="Enter Email Adress" type="email" required>
-                    <button class="btnFooter" type="submit"><i class="fa fa-arrow-right"></i></button>
-                </form>
-            </div>
-            <div class="col-12 col-sm-12 col-lg-2 ">
-                <div class="footerFollow">
-                    <h4>Follow Me</h4>
-                    <p>Let us bes social</p>
-                    <div class="footerSocials">
-                        <a href="#" class="fa fa-facebook" target="_blank"></a>
-                        <a href="#" class="fa fa-twitter" target="_blank"></a>
-                        <a href="#" class="fa fa-dribbble" target="_blank"></a>
-                        <a href="#" class="fa fa-behance" target="_blank"></a>
-                    </div>
-                </div>
-            </div>`
-}
+    </div>
+</div>`
+} */
